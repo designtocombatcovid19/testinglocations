895f45701429165baf2f715d6303331b58d38ffa
Branden Byers <branden@brandenbyers.com>
2020-04-11 21:54:57 -0700

    Change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': ['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+    },
+    colors: {
+      blue: '#DCE4EF'
     }
   },
   variants: {},
