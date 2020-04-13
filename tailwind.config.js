fd247dd67726c1f70c43a1b966b8c713c7673688
Branden Byers <branden@brandenbyers.com>
2020-04-12 20:58:56 -0700

    Quick check

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
       'sans': ['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
     },
     colors: {
-      blue: '#DCE4EF',
+      blue: '#6638f0',
       red: '#f60033'
     }
   },
