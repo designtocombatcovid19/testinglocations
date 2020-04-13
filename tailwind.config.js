cc6f3b2ce31896c0c55943b0659fa23182d5db2e
Branden Byers <branden@brandenbyers.com>
2020-04-12 21:07:28 -0700

    Revert color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
       'sans': ['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
     },
     colors: {
-      blue: '#6638f0',
+      blue: '#DCE4EF',
       red: '#f60033'
     }
   },
